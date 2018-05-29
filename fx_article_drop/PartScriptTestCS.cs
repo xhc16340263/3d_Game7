7c7e4a29cab4e22fbef999fd7f26044c5a7e599b
xhc16340263 <31815522+xhc16340263@users.noreply.github.com>
2018-05-29 22:09:19 +0800

    Add files via upload

---
+++ fx_article_drop/PartScriptTestCS.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartScriptTestCS : MonoBehaviour {
+
+	public float engineRevs;
+	public float exhaustRate;
+
+	ParticleSystem exhaust;
+
+
+	void Start () {
+		exhaust = GetComponent<ParticleSystem>();
+	}
+
+
+	void Update () {
+		exhaust.emissionRate = engineRevs * exhaustRate;
+	}
+
+}
